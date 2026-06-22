9841fca61975b43dcde41e82c52dceb7e06b2de4
Gavin Chaineux <gavin.chaineux@unamur.be>
2026-06-22 15:00:09 +0200

    MEmberData

---
+++ testingApp/Panier.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace testingApp
+{
+    public enum TypeClient
+    {
+        Standard,
+        Premium,
+        VIP,
+        GuideSupreme
+    }
+
+    public class Article
+    {
+        public string Nom { get; set; }
+        public decimal Prix { get; set; }
+        public Article(string nom, decimal prix)
+        {
+            Nom = nom;
+            Prix = prix;
+        }
+    }
+
+    public class Client
+    {
+        public string Nom { get; set; }
+        public TypeClient Status { get; set; }
+        public Client(string nom, TypeClient status)
+        {
+            Nom = nom;
+            Status = status;
+        }
+    }
+
+    public class Panier
+    {
+        public Client Client { get; set; }
+
+        public List<Article> Articles { get; set; } = new List<Article>();
+
+        public decimal CalculerTotal()
+        {
+            decimal total = 0;
+
+            foreach (var article in Articles)
+            { 
+                total += article.Prix;
+            }
+
+            switch (Client.Status)
+            {
+                case TypeClient.Premium:
+                    total *= 0.9m; // 10% de réduction
+                    break;
+                case TypeClient.VIP:
+                    total *= 0.8m; // 20% de réduction
+                    break;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ testingAppTests/PanierTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using testingApp;
+
+namespace testingAppTests
+{
+    public class PanierTests
+    {
+        [Theory]
+        [MemberData(nameof(PanierCalculerTotal_TestData))]
+        public void PanierCalculerTotal_SelonStatus_RetournePrixCorrect(Panier p, Decimal expected)
+        {
+            decimal total = p.CalculerTotal();
+
+            Assert.Equal(expected, total);
+        }
+
+        public static IEnumerable<object[]> PanierCalculerTotal_TestData()
+        {
+            yield return new object[]
+            {
+                new Panier
+                {
+                    Client = new Client("Georges", TypeClient.Standard),
+                    Articles = new List<Article>{new Article("Banane", 2.50m), new Article("Pomme", 5), new Article("Raisins", 1.2m)}
+                },
+                8.7m
+            };
+            yield return new object[]
+            {
+                new Panier
+                {
+                    Client = new Client("Georgette", TypeClient.Premium),
+                    Articles = new List<Article>{new Article("Banane", 2.50m), new Article("Pomme", 5), new Article("Raisins", 6m)}
+                },
+                12.15m
+            };
+            yield return new object[]
+            {
+                new Panier
+                {
+                    Client = new Client("Georgette", TypeClient.VIP),
+                    Articles = new List<Article>{new Article("Banane", 12.5m), new Article("Pomme", 5), new Article("Raisins", 6m)}
+                },
+                18.8m
+            };
+        }
+    }
+}
