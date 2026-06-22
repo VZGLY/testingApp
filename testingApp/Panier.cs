bba8c66764391ed47a693ceaa3e2a746dd3f05da
Gavin Chaineux <gavin.chaineux@unamur.be>
2026-06-22 15:20:29 +0200

    Exo 3

--- testingApp/Panier.cs
+++ testingApp/Panier.cs
@@ -62,5 +62,95 @@ namespace testingApp
 
             return total;
         }
+
+        public void AjouterArticle(Article article)
+        {
+            if (article == null)
+                throw new ArgumentNullException(nameof(article));
+
+            Articles.Add(article);
+        }
+
+        public bool SupprimerArticle(string nom)
+        {
+            for (int i = 0; i < Articles.Count; i++)
+            {
+                if (Articles[i].Nom == nom)
+                {
+                    Articles.RemoveAt(i);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int NombreArticles()
+        {
+            return Articles.Count;
+        }
+
+        public Article ArticleLePlusCher()
+        {
+            Article plusCher = Articles[0];
+
+            foreach (var article in Articles)
+            {
+                if (article.Prix < plusCher.Prix)
+                {
+                    plusCher = article;
+                }
+            }
+
+            return plusCher;
+        }
+
+        public decimal PrixMoyen()
+        {
+            decimal total = 0;
+
+            foreach (var article in Articles)
+            {
+                total += article.Prix;
+            }
+
+            return total / Articles.Count;
+        }
+
+        public decimal CalculerTotalAvecTVA(decimal tauxTVA)
+        {
+            decimal sousTotal = 0;
+
+            foreach (var article in Articles)
+            {
+                sousTotal += article.Prix;
+            }
+
+            decimal tva = sousTotal * tauxTVA / 100;
+
+            return CalculerTotal() + tva;
+        }
+
+        public decimal AppliquerCodePromo(string code)
+        {
+            decimal total = CalculerTotal();
+
+            switch (code)
+            {
+                case "PROMO10":
+                    total -= total * 0.10m;
+                    break;
+                case "PROMO20":
+                    total -= total * 0.20m;
+                    break;
+            }
+
+            return total;
+        }
+
+        public bool EstEligibleLivraisonGratuite()
+        {
+            return CalculerTotal() > 50m;
+        }
     }
 }
